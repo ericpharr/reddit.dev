6d485143e398155eb37d2c9b6ec1fb74f9f894aa
ericpharr <ericjpharr@gmail.com>
2017-08-31 10:01:10 -0500

    Added navbar and routed top-level domain route to /posts.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostsController@index');
 
 Route::get('/uppercase/{word}', 'HomeController@uppercase');
 
